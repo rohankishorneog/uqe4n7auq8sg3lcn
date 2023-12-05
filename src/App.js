86b0d9afd3576e8211a2112c5cccf11d4422c6a9
rohankishorneog <rkn2601@gmail.com>
2023-12-05 15:27:27 +0530

    adedd the route to the link for calculator page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router";
 import LandingPage from "./Pages/LandingPage/LandingPage";
+import BmiCalculationPage from "./Pages/BmiCalculationPage/BmiCalculationPage";
 
 function App() {
   return (
     <Routes>
       <Route exact path="/"  element={<LandingPage/>}></Route>
+      <Route exact path="/bmiCalculator" element={<BmiCalculationPage/>}></Route>
     </Routes>
   );
 }
