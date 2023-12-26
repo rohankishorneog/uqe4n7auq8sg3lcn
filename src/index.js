c7fb3ae6f8210110a2975bc90f27e9245c2a08cf
rohankishorneog <rkn2601@gmail.com>
2023-12-26 22:50:44 +0530

    rpleaced hashrouter with browser-router

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { HashRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router >
+    <Router basename='/uqe4n7auq8sg3lcn'>
       <App/>
     </Router>
   </React.StrictMode>
